d92277347af12b3f02816635f71f62d641db339f
Darren Bane <darren.bane@emdalo.com>
2018-01-09 14:55:32 +0000

    Some doxygen

--- src/pdcenv.h
+++ src/pdcenv.h
@@ -15,9 +15,16 @@
 #ifndef PDCENV_H
 #define PDCENV_H
 
+/** Create a new environment */
 extern struct comal_env *env_new(const char *name);
+
+/** Return a named environment */
 extern struct comal_env *env_find(char *name);
+
+/** Return the current program to its initial state */
 extern void clean_runenv(struct comal_env *env);
+
+/** Totally clear an environment, including the current program */
 extern void clear_env(struct comal_env *env);
 
 #endif
--- src/pdcexec.h
+++ src/pdcexec.h
@@ -15,20 +15,45 @@
 #ifndef PDCEXEC_H
 #define PDCEXEC_H
 
+/** Signal a run-time error  */
 extern void run_error(int error, const char *s, ...);
+
+/** Call a PROC/FUNC definition */
 extern void exec_call(struct expression *exp, int calltype, void **result,
 		      enum VAL_TYPE *type);
+
+/** Install an exception handler */
 extern int exec_trap(struct comal_line *line);
+
+/** Return the metadata for an open file */
 extern struct file_rec *fsearch(long i);
+
+/** Read from a binary format file */
 extern void do_readfile(struct two_exp *twoexp, struct exp_list *lvalroot);
+
+/** Read from in-program DATA statements */
 extern void read_data(struct comal_line *line);
+
+/** Implement the READ keyword */
 extern void exec_read(struct comal_line *line);
+
+/** Write to a binary format file */
 extern void exec_write(struct comal_line *line);
+
+/** Print to a text format file */
 extern void print_file(struct two_exp *twoexp,
 		       struct print_list *printroot, int pr_sep, struct expression *using_modifier);
+
+/** Input from a text format file */
 extern void input_file(struct two_exp *twoexp, struct exp_list *lvalroot);
+
+/** Execute one line of the program */
 extern int exec_line(struct comal_line *line);
+
+/** Execute a sequence of lines, in one environment */
 extern void exec_seq(struct comal_line *line);
+
+/** Initialise any module initialisation statements */
 extern void exec_mod_init(struct comal_line *line);
 
 #endif
